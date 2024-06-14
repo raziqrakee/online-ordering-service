2700bb12140ae9f24eb6b5adeeb8581fd1b57407
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2024-06-15 02:24:11 +0800

    [CREATED] SalesReport logic and endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,13 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\CateringServiceController;
 use App\Http\Controllers\API\ReservationController;
-
+use App\Http\Controllers\API\SalesReportController;
 
 // User
 Route::post('register', [AuthController::class, 'register']);
@@ -43,17 +44,24 @@ Route::post('product/{id}', [ProductController::class, 'product_update']);
 Route::delete('products/{id}/delete', [ProductController::class, 'p_destroy']);
 Route::post('products/{id}/purchase', [ProductController::class, 'p_purchase']);
 
-// Order
-Route::get('orders', [OrderController::class, 'index']);
-Route::post('orders', [OrderController::class, 'store']);
-Route::get('orders/{id}', [OrderController::class, 'show']);
-Route::put('orders/{id}', [OrderController::class, 'update']);
-Route::delete('orders/{id}', [OrderController::class, 'destroy']);
-
+// Catering Service
 // Route::middleware('auth:sanctum')->post('/catering-service', [CateringServiceController::class, 'submitForm']);
 Route::post('/catering-service', [CateringServiceController::class, 'submitForm']);
-
 
 // Reservation
 Route::apiResource('reservations', ReservationController::class);
 Route::get('available-slots/{date}', [ReservationController::class, 'availableSlots']);
+
+// Order
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('orders', [OrderController::class, 'placeOrder']);
+    Route::put('orders/{id}/status', [OrderController::class, 'updateOrderStatus']);
+    Route::get('orders/{id}', [OrderController::class, 'viewOrder']);
+    Route::get('orders', [OrderController::class, 'listOrders']);
+    Route::get('orders/{id}/details', [OrderController::class, 'viewOrderDetails']);
+    Route::get('orders/latest', [OrderController::class, 'getLatestOrderId']);
+});
+
+// Sales Report
+Route::get('sales-reports', [SalesReportController::class, 'index']);
+
